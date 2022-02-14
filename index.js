378171ec40c15bffdaaa88559ced4be0333162ce
Trần Nhật Minh Đặng <nhatminh13082004@gmail.com>
2022-02-14 22:51:19 +1100

    consistent formatting enforce

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow () {
-  const win = new BrowserWindow({
-    backgroundColor: '#000000',
-    width: 1000,
-    height: 700,
-    minWidth: 550,
-    minHeight: 600,
-    titleBarStyle: "default"
-  })
+function createWindow() {
+	const win = new BrowserWindow({
+		backgroundColor: '#000000',
+		width: 1000,
+		height: 700,
+		minWidth: 550,
+		minHeight: 600,
+		titleBarStyle: "default",
+	});
 
-  win.loadFile('index.html')
+	win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow()
+	createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
